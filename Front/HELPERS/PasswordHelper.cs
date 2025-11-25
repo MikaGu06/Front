93fd293db3a1d789666fe8e33ccdf39814294eba
diego_dell <diegoaprende06@gmail.com>
2025-11-24 22:54:28 -0400

    -uniendo base de datos - creando hash para las contraseñas

---
+++ Front/HELPERS/PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Front.Helpers
+{
+    public static class PasswordHelper
+    {
+        // Convierte la contraseña en hash SHA256
+        public static byte[] HashPassword(string password)
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                return sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        // Convierte el hash (bytes) a formato hexadecimal para SQL Server
+        public static string HashToHex(byte[] hash)
+        {
+            return "0x" + BitConverter.ToString(hash).Replace("-", "");
+        }
+    }
+}
