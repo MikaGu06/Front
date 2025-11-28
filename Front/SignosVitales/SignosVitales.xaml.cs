1240f36f0045af21346d0228203921a526f39061
Mika <mikaelagutierrezurquiza@gmail.com>
2025-11-28 09:05:21 -0400

    part2

--- Front/SignosVitales/SignosVitales.xaml.cs
+++ Front/SignosVitales/SignosVitales.xaml.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using Front.SignosVitales;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Front
 {
+    /// <summary>
+    /// Lógica de interacción para SignosVitales.xaml
+    /// </summary>
     public partial class SignosVitales : Page
     {
         public SignosVitales()
@@ -12,50 +24,9 @@ namespace Front
             InitializeComponent();
         }
 
-        private void BtnVolver_Click(object sender, RoutedEventArgs e)
-        {
-            ((MainWindow)Application.Current.MainWindow).MainFrame.Navigate(new Servicios());
-        }
+       
+     
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                // Llamamos directamente a la clase de validación
-                ValidacionSignosVitales.Validar(
-                    txtRitmoCardiaco.Text,
-                    txtPresionArterial.Text,
-                    txtOxigenacion.Text,
-                    txtTemperatura.Text
-                );
-
-                // Si no lanzó excepción → todo correcto
-                MessageBox.Show("Datos añadidos correctamente.",
-                                "Registro exitoso",
-                                MessageBoxButton.OK,
-                                MessageBoxImage.Information);
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show("ERROR de Validación: " + ex.Message,
-                                "Datos Inválidos",
-                                MessageBoxButton.OK,
-                                MessageBoxImage.Warning);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("ERROR de Formato: Uno o más valores numéricos no son válidos.",
-                                "Datos Inválidos",
-                                MessageBoxButton.OK,
-                                MessageBoxImage.Warning);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("El sistema ha fallado. Reporte el código de error: " + ex.Message,
-                                "Fallo Crítico",
-                                MessageBoxButton.OK,
-                                MessageBoxImage.Error);
-            }
-        }
+        
     }
 }
